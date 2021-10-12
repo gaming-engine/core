d322736b9ffcae05d56b4c134d7bd547d16c9a62
Andrew Judd <awjudd@gmail.com>
2021-10-12 11:56:41 +0000

    User Model Issues

--- src/Account/Entities/CoreUser.php
+++ src/Account/Entities/CoreUser.php
@@ -4,8 +4,18 @@ namespace GamingEngine\Core\Account\Entities;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
 
 class CoreUser extends User
 {
     use SoftDeletes;
+
+    protected $table = 'users';
+
+    protected $guarded = [];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = Hash::make($password);
+    }
 }
